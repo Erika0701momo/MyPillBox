8d20183923476dd03c926ccee1c26b3d57d5ea31
Erika Inoue <kinako771@gmail.com>
2025-01-09 13:49:57 +0900

    デプロイ準備

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "").replace(
+        "postgres://", "postgresql://"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT")
     # daily_logs.htmlで表示する日々の記録の数を設定
     LOGS_PER_PAGE = 10
     LANGUAGES = ["ja", "en"]
